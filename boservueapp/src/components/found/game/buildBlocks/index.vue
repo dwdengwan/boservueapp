b5219aa27e8c35bd1d98d98bc0bfb33157b752bb
jzcz38 <jzcz38@bosera.com>
2020-09-23 14:14:41 +0800

    引入echart

--- boservueapp/src/components/found/game/buildBlocks/index.vue
+++ boservueapp/src/components/found/game/buildBlocks/index.vue
@@ -3,7 +3,9 @@
         <div class="header build-blocks-header">
             <go-back-header></go-back-header>
         </div>
-        <div class="content build-blocks-content"></div>
+        <div class="content build-blocks-content">
+            <div id="myChart"></div>
+        </div>
         <div class="footer build-blocks-footer">
             <game-footer></game-footer>
         </div>
@@ -19,9 +21,88 @@
             goBackHeader,
             gameFooter
         },
+        methods:{
+            getEcharts(){
+                //初始化echart对象
+                let myChart = this.$echarts.init(document.getElementById('myChart'));
+                //配置echart
+                myChart.setOption({
+                    grid: {//整体位置
+                        top: '40',
+                        right: '8%',
+                        left: '12%',
+                        bottom: 36,
+                    },
+                    backgroundColor: '#fff',
+                    xAxis: {
+                        type: 'value',//类型--数值轴
+                        position: 'top',//位置
+                        boundaryGap : 0,//坐标轴两侧留白
+                        offset: 8,//X轴偏移
+                        axisLine : {//坐标轴轴线
+                            show: 0,
+                        },
+                        axisTick : {show: 0},//坐标轴刻度
+                        splitLine : {show: 0},//坐标轴grid区分割线
+                        //data: ['10', '20', '30', '40'],//类目轴有效
+                    },
+                    yAxis: {
+                        type: 'category',//类型--类目轴
+                        name:'期数',
+                        nameLocation: 'start', //轴名位置
+                        nameTextStyle: {//Y轴名称
+                            color: '#f00',
+                        },
+                        offset: 8,//Y轴偏移
+                        nameGap: 15,//轴名距离
+                        inverse: 1,//反转
+                        boundaryGap : 0,//坐标轴两侧留白
+                        axisLine : {show: 0},//坐标轴轴线
+                        axisTick : {show: 0},//坐标轴刻度
+                        splitLine : {//坐标轴grid区分割线
+                            show: 1,
+                            lineStyle:{
+                                color: '#dcdcdc'
+                            }
+                        },
+                        data: [1,2,3,4],//y轴刻度
+                    },
+                    series: [{
+                        data: [23,24,25,26],//数据--将按照类目表依次排列
+                        type: 'line',//折线图
+                        label: {//标签
+                            show: 1,
+                            position: 'insideTopLeft',
+                            distance: 12,
+                            color: '#000',
+                            fontSize: 14,
+                        },
+                        itemStyle:{//标记拐点颜色
+                            // color: (param)=>{
+                            //     return function(param){
+                            //         console.log(param)
+                            //     }
+                            // }
+                            color:'#0f0',
+                        },
+                        symbol: 'circle',//标记-实心圆
+                        symbolSize: 10,//标记-大小
+                        lineStyle:{color:'#ccc',},//line样式
+                    }]
+                });
+            },
+        },
+        mounted(){
+            this.getEcharts()
+        }
     }
 </script>
 
 <style scoped lang="less">
-
+.build-blocks-content{
+    #myChart{
+        width: 100%;
+        height: 100%;
+    }
+}
 </style>
--- boservueapp/src/main.js
+++ boservueapp/src/main.js
@@ -11,6 +11,9 @@ import '../src/static/css/global.css' /*引入公共样式*/
 import common from '../src/static/js/global.js' /*引入公共方法*/
 import  'amfe-flexible' //引入rem
 
+import echarts from 'echarts' //引入echarts
+Vue.prototype.$echarts = echarts //引入组件
+
 Vue.prototype.$common = common;
 Vue.config.productionTip = false
 
